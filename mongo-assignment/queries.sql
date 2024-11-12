7ee208c6f9d227b14703be1084efc4a511e4774e
Nishant <nishant.e.ekka@gmail.com>
2024-11-12 22:14:33 +0530

    Completed till 3 in mongodb

--- mongo-assignment/queries.sql
+++ mongo-assignment/queries.sql
@@ -173,3 +173,336 @@ db.orders.updateOne({"order_id": 1}, {$set: {"status": "delivered"}});
 db.orders.deleteOne({"order_id": 1});
 -- output
 -- { acknowledged: true, deletedCount: 1 }
+
+
+-- 2.1 Write a script to calculate the total value of all orders for each customer. This should return each customer’s name and the total order value.
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "customers",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer"
+    }
+  },
+  {
+    $unwind: "$customer"
+  },
+  {
+    $group: {
+      _id: "$customer.name",
+      total_order_value: { $sum: "$total_value" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      customer_name: "$_id",
+      total_order_value: 1
+    }
+  }
+]);
+
+-- output
+-- [
+--   { total_order_value: 750, customer_name: 'Eve Davis' },
+--   { total_order_value: 150, customer_name: 'Jane Smith' },
+--   { total_order_value: 550, customer_name: 'Bob Brown' },
+--   { total_order_value: 350, customer_name: 'Alice Johnson' }
+-- ]
+
+-- 2.2 Write a script to group orders by their status (e.g., “shipped”, “delivered”, etc.) and count how many orders are in each status.
+
+db.orders.aggregate([
+  {
+    $group: {
+      _id: "$status",
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      status: "$_id",
+      count: 1
+    }
+  }
+]);
+
+-- output
+-- [ { count: 4, status: 'shipped' } ]
+
+--2.3 Write a script to find each customer and their most recent order. Include customer information such as name, email, and order details (e.g., order_id, total_value).
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "customers",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer"
+    }
+  },
+  {
+    $unwind: "$customer"
+  },
+  {
+    $sort: { order_date: -1 }
+  },
+  {
+    $group: {
+      _id: "$customer.name",
+      email: { $first: "$customer.email" },
+      order: {
+        $first: {
+          order_id: "$order_id",
+          total_value: "$total_value"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      customer_name: "$_id",
+      email: 1,
+      order: 1
+    }
+  }
+]);
+
+-- output
+-- [
+--   {
+--     email: 'alicejhonson@example.com',
+--     order: { order_id: 3, total_value: 350 },
+--     customer_name: 'Alice Johnson'
+--   },
+--   {
+--     email: 'bobbrown@example.com',
+--     order: { order_id: 4, total_value: 550 },
+--     customer_name: 'Bob Brown'
+--   },
+--   {
+--     email: 'janesmith@example.com',
+--     order: { order_id: 2, total_value: 150 },
+--     customer_name: 'Jane Smith'
+--   },
+--   {
+--     email: 'evedavis@example.com',
+--     order: { order_id: 5, total_value: 750 },
+--     customer_name: 'Eve Davis'
+--   }
+-- ]
+
+-- 2.4 Write a script to find the most expensive order for each customer. Return the customer’s name and the details of their most expensive order (e.g., order_id, total_value).
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "customers",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer"
+    }
+  },
+  {
+    $unwind: "$customer"
+  },
+  {
+    $sort: { total_value: -1 }
+  },
+  {
+    $group: {
+      _id: "$customer.name",
+      order: {
+        $first: {
+          order_id: "$order_id",
+          total_value: "$total_value"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      customer_name: "$_id",
+      order: 1
+    }
+  }
+]);
+
+--output
+
+-- [
+--   {
+--     order: { order_id: 3, total_value: 350 },
+--     customer_name: 'Alice Johnson'
+--   },
+--   {
+--     order: { order_id: 4, total_value: 550 },
+--     customer_name: 'Bob Brown'
+--   },
+--   {
+--     order: { order_id: 2, total_value: 150 },
+--     customer_name: 'Jane Smith'
+--   },
+--   {
+--     order: { order_id: 5, total_value: 750 },
+--     customer_name: 'Eve Davis'
+--   }
+-- ]
+
+-- 3.1 Write a script to find all customers who have placed at least one order in the last 30 days. Return the customer name, email, and the order date for their most recent order.
+
+db.orders.aggregate([
+  {
+    $match: {
+      order_date: {
+        $gte: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000)
+      }
+    }
+  },
+  {
+    $lookup: {
+      from: "customers",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer"
+    }
+  },
+  {
+    $unwind: "$customer"
+  },
+  {
+    $sort: { order_date: -1 }
+  },
+  {
+    $group: {
+      _id: "$customer.name",
+      email: { $first: "$customer.email" },
+      order: {
+        $first: {
+          order_date: "$order_date"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      customer_name: "$_id",
+      email: 1,
+      order: 1
+    }
+  }
+]);
+
+--3.2 Write a script to find all distinct products ordered by a customer with the name “John Doe”. Include the product name and the total quantity ordered.
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "customers",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer"
+    }
+  },
+  {
+    $unwind: "$customer"
+  },
+  {
+    $match: {
+      "customer.name": "John Doe"
+    }
+  },
+  {
+    $unwind: "$items"
+  },
+  {
+    $group: {
+      _id: "$items.product_name",
+      total_quantity: { $sum: "$items.quantity" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      product_name: "$_id",
+      total_quantity: 1
+    }
+  }
+]);
+
+--output
+-- [
+--   { total_quantity: 1, product_name: 'product2' },
+--   { total_quantity: 2, product_name: 'product1' }
+-- ]
+
+--3.3 Write a script to find the top 3 customers who have spent the most on orders. Sort the results by total order value in descending order.
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "customers",
+      localField: "customer_id",
+      foreignField: "_id",
+      as: "customer"
+    }
+  },
+  {
+    $unwind: "$customer"
+  },
+  {
+    $group: {
+      _id: "$customer.name",
+      total_order_value: { $sum: "$total_value" }
+    }
+  },
+  {
+    $sort: { total_order_value: -1 }
+  },
+  {
+    $limit: 3
+  },
+  {
+    $project: {
+      _id: 0,
+      customer_name: "$_id",
+      total_order_value: 1
+    }
+  }
+]);
+
+--output
+
+-- [
+--   { total_order_value: 750, customer_name: 'Eve Davis' },
+--   { total_order_value: 550, customer_name: 'Bob Brown' },
+--   { total_order_value: 350, customer_name: 'Alice Johnson' }
+-- ]
+
+--3.4 Write a script to add a new order for a customer with the name “Jane Smith”. The order should include at least two items, such as “Smartphone” and “Headphones”.
+
+const customer_id = db.customers.findOne({ "name": "Jane Smith" })._id;
+db.orders.insertOne({
+  "order_id": 6,
+  "customer_id": customer_id,
+  "order_date": ISODate("2023-01-01T12:00:00Z"),
+  "status": "shipped",
+  "items": [
+    { "product_name": "Smartphone", "quantity": 1, "price": 500.00 },
+    { "product_name": "Headphones", "quantity": 1, "price": 100.00 }
+  ],
+  "total_value": 600.00
+});
+
+--output
+
+-- {
+--   acknowledged: true,
+--   insertedId: ObjectId('673385ca809c8ae19c0d81a4')
+-- }
